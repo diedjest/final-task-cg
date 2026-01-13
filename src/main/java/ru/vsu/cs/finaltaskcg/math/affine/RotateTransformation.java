00b0b4b785891d64f27721178fced6a158f8dd34
Dmitriy Uvarov <uvarov.d2007@mail.ru>
2026-01-13 21:40:42 +0300

    Refactor RotateTransformation.java from default matrix realisation to own realisation

--- src/main/java/ru/vsu/cs/finaltaskcg/math/affine/RotateTransformation.java
+++ src/main/java/ru/vsu/cs/finaltaskcg/math/affine/RotateTransformation.java
@@ -1,47 +1,45 @@
 package ru.vsu.cs.finaltaskcg.math.affine;
 
-import javax.vecmath.Matrix4d;
-import javax.vecmath.Point3d;
+import ru.vsu.cs.finaltaskcg.math.matrix.Matrix4;
 
 public class RotateTransformation implements Transformation {
-    private final Matrix4d rotation;
+    private final Matrix4 rotation;
 
     public RotateTransformation(Axis axis, double angle) {
-       this.rotation =  createRotationMatrix(axis, angle);
+       this.rotation = createRotationMatrix(axis, angle);
     }
 
-    private Matrix4d createRotationMatrix(Axis axis, double angle) {
+    private Matrix4 createRotationMatrix(Axis axis, double angle) {
+        double[][] xRotationMatrix = new double[][] {
+                {1, 0, 0, 0},
+                {0, Math.cos(angle), Math.sin(angle), 0},
+                {0, -Math.sin(angle), Math.cos(angle), 0},
+                {0, 0, 0, 1}};
+        double[][] yRotationMatrix = new double[][] {
+                {Math.cos(angle), 0, Math.sin(angle), 0},
+                {0, 1, 0, 0},
+                {-Math.sin(angle), 0, Math.cos(angle), 0},
+                {0, 0, 0, 1}
+        };
+        double[][] zRotationMatrix = new double[][] {
+                {Math.cos(angle), Math.sin(angle), 0, 0},
+                {-Math.sin(angle), Math.cos(angle), 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
         switch (axis) {
             case X:
-                return new Matrix4d(
-                        1, 0, 0, 0,
-                        0, Math.cos(angle), Math.sin(angle), 0,
-                        0, -Math.sin(angle), Math.cos(angle), 0,
-                        0, 0, 0, 1
-                );
+                return new Matrix4(xRotationMatrix);
             case Y:
-                return new Matrix4d(
-                        Math.cos(angle), 0, Math.sin(angle), 0,
-                        0, 1, 0, 0,
-                        -Math.sin(angle), 0, Math.cos(angle), 0,
-                        0, 0, 0, 1
-                );
+                return new Matrix4(yRotationMatrix);
             case Z:
-                return new Matrix4d(
-                        Math.cos(angle), Math.sin(angle), 0, 0,
-                        -Math.sin(angle), Math.cos(angle), 0, 0,
-                        0, 0, 1, 0,
-                        0, 0, 0, 1
-                );
+                return new Matrix4(zRotationMatrix);
             default:
                 throw new IllegalArgumentException("Unknown axis:" + axis);
         }
     }
 
-    @Override
-    public Matrix4d getMatrix() {
-        Matrix4d matrix = new Matrix4d();
-        matrix.set(rotation);
-        return matrix;
+    public Matrix4 getMatrix() {
+        return rotation;
     }
 }
