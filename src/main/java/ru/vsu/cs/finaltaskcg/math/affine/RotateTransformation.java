a3f15e7bb12b70a1591a78fdc5b56ddac99b901f
Dmitriy Uvarov <uvarov.d2007@mail.ru>
2026-01-13 22:09:23 +0300

    Refactor RotateTransformation.java, remove temporary matrices

--- src/main/java/ru/vsu/cs/finaltaskcg/math/affine/RotateTransformation.java
+++ src/main/java/ru/vsu/cs/finaltaskcg/math/affine/RotateTransformation.java
@@ -6,40 +6,39 @@ public class RotateTransformation implements Transformation {
     private final Matrix4 rotation;
 
     public RotateTransformation(Axis axis, double angle) {
-       this.rotation = createRotationMatrix(axis, angle);
+        this.rotation = createRotationMatrix(axis, angle);
     }
 
     private Matrix4 createRotationMatrix(Axis axis, double angle) {
-        double[][] xRotationMatrix = {
-                {1, 0, 0, 0},
-                {0, Math.cos(angle), Math.sin(angle), 0},
-                {0, -Math.sin(angle), Math.cos(angle), 0},
-                {0, 0, 0, 1}
-        };
-        double[][] yRotationMatrix = {
-                {Math.cos(angle), 0, Math.sin(angle), 0},
-                {0, 1, 0, 0},
-                {-Math.sin(angle), 0, Math.cos(angle), 0},
-                {0, 0, 0, 1}
-        };
-        double[][] zRotationMatrix = {
-                {Math.cos(angle), Math.sin(angle), 0, 0},
-                {-Math.sin(angle), Math.cos(angle), 0, 0},
-                {0, 0, 1, 0},
-                {0, 0, 0, 1}
-        };
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+
+        Matrix4 matrix = Matrix4.identity();
+
         switch (axis) {
             case X:
-                return new Matrix4(xRotationMatrix);
+                matrix.set(1, 1, cos);  matrix.set(1, 2, -sin);
+                matrix.set(2, 1, sin);  matrix.set(2, 2, cos);
+                break;
+
             case Y:
-                return new Matrix4(yRotationMatrix);
+                matrix.set(0, 0, cos);  matrix.set(0, 2, sin);
+                matrix.set(2, 0, -sin); matrix.set(2, 2, cos);
+                break;
+
             case Z:
-                return new Matrix4(zRotationMatrix);
+                matrix.set(0, 0, cos);  matrix.set(0, 1, -sin);
+                matrix.set(1, 0, sin);  matrix.set(1, 1, cos);
+                break;
+
             default:
-                throw new IllegalArgumentException("Unknown axis:" + axis);
+                throw new IllegalArgumentException("Unknown axis: " + axis);
         }
+
+        return matrix;
     }
 
+    @Override
     public Matrix4 getMatrix() {
         return rotation;
     }
