18d1b6b6f26a71c70fd75db74a862b59acb17c6e
Dmitriy Uvarov <uvarov.d2007@mail.ru>
2026-01-13 17:16:28 +0300

    Add validation checking for Vector3

--- src/main/java/ru/vsu/cs/finaltaskcg/math/vector/Vector3.java
+++ src/main/java/ru/vsu/cs/finaltaskcg/math/vector/Vector3.java
@@ -1,6 +1,7 @@
 package ru.vsu.cs.finaltaskcg.math.vector;
 
 import ru.vsu.cs.finaltaskcg.math.exceptions.MathException;
+import ru.vsu.cs.finaltaskcg.math.validation.MathValidator;
 
 public class Vector3 {
     private double x, y, z;
@@ -36,10 +37,12 @@ public class Vector3 {
     }
 
     public Vector3 add(Vector3 v) {
+        MathValidator.checkNotNull(v, "Vector3 for addition");
         return new Vector3(x + v.x, y + v.y, z + v.z);
     }
 
     public Vector3 sub(Vector3 v) {
+        MathValidator.checkNotNull(v, "Vector3 for subtraction");
         return new Vector3(x - v.x, y - v.y, z - v.z);
     }
 
@@ -48,7 +51,7 @@ public class Vector3 {
     }
 
     public Vector3 div(double s) {
-        if (s == 0) throw new MathException("Divide by zero");
+        MathValidator.checkNotZero(s, "Divisor");
         return new Vector3(x / s, y / s, z / s);
     }
 
@@ -58,15 +61,17 @@ public class Vector3 {
 
     public Vector3 normalize() {
         double len = length();
-        if (len == 0) throw new MathException("Cannot normalize zero vector");
+        MathValidator.checkNotZero(len, "Vector length for normalization");
         return div(len);
     }
 
     public double dot(Vector3 v) {
+        MathValidator.checkNotNull(v, "Vector3 for dot product");
         return x*v.x + y*v.y + z*v.z;
     }
 
     public Vector3 cross(Vector3 v) {
+        MathValidator.checkNotNull(v, "Vector3 for cross product");
         return new Vector3(
                 y * v.z - z * v.y,
                 z * v.x - x * v.z,
