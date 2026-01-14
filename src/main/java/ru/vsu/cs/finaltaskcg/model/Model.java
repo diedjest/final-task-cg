57eb6bd26ae87a8b5c440d680bcdec7c696ec111
Dmitriy Uvarov <uvarov.d2007@mail.ru>
2026-01-14 19:21:19 +0300

    Change Vector3f and Vector2f to own realisation Vector3 and Vector2 at Model.java

--- src/main/java/ru/vsu/cs/finaltaskcg/model/Model.java
+++ src/main/java/ru/vsu/cs/finaltaskcg/model/Model.java
@@ -1,13 +1,14 @@
 package ru.vsu.cs.finaltaskcg.model;
-import com.cgvsu.math.Vector2f;
-import com.cgvsu.math.Vector3f;
+
+import ru.vsu.cs.finaltaskcg.math.vector.Vector2;
+import ru.vsu.cs.finaltaskcg.math.vector.Vector3;
 
 import java.util.*;
 
 public class Model {
 
-    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
-    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
-    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
+    public ArrayList<Vector3> vertices = new ArrayList<Vector3>();
+    public ArrayList<Vector2> textureVertices = new ArrayList<Vector2>();
+    public ArrayList<Vector3> normals = new ArrayList<Vector3>();
     public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
 }
